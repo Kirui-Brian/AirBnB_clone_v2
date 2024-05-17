32e28d155cece22619cfec53f5f0dda07be8559d
Kirui Brian <brinkiru@gmail.com>
2024-05-17 12:33:20 +0300

    add executable 7-states_list.py file

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains a Flask instance.
+
+The instance listena on 0.0.0.0, port 5000.
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    This function returns a HTML page when accessing the '/states_list' path.
+    The page contains a list of all State objects present in DBStorage sorted by name (A->Z).
+    """
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+@app.teardown_appcontext
+def close_session(exception):
+    """
+    This function removes the current SQLAlchemy Session after each request.
+    """
+    storage.close()
+
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
