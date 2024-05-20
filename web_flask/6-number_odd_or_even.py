e91af45a2f063057c95abbbb212f21f7646df420
Kirui Brian <brinkiru@gmail.com>
2024-05-20 11:40:33 +0300

    add 6-number_odd_or_even.py that conforms with PEP 8 style

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -2,11 +2,13 @@
 """
 This module contains a Flask instance.
 
-The instance listena on 0.0.0.0, port 5000.
+The instance listens on 0.0.0.0, port 5000.
 """
 
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -15,12 +17,14 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
-    This function returns the string "HBNB" wehn accessing the '/hbnb' path.
+    This function returns the string "HBNB" when accessing the '/hbnb' path.
     """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
@@ -29,6 +33,7 @@ def c_text(text):
     Underscore symbols in the text variable are replaced with a space.
     """
     return "C " + text.replace("_", " ")
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -40,6 +45,7 @@ def python_text(text):
     """
     return "Python " + text.replace("_", " ")
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
     """
@@ -47,6 +53,7 @@ def number_n(n):
     This route only works if n is an integer.
     """
     return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
@@ -56,6 +63,7 @@ def number_template(n):
     """
     return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """
@@ -63,6 +71,7 @@ def number_odd_or_even(n):
     This route only works if n is an integer.
     """
     return render_template('6-number_odd_or_even.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
