854b76463c42f1854503b5dc94777b29cdc45849
Kirui Brian <brinkiru@gmail.com>
2024-05-20 18:21:52 +0300

    add db_storage.py file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,4 +2,4 @@ def close(self):
     """
     Calls remove() method on the private session attribute (self.__session) or close() on the class Session.
     """
-    self.__session.close()
+    self.__session.remove()
