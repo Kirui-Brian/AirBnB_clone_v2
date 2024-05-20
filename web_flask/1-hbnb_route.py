dbe0c84f46fdd1ed0d59b3572438fe27d1f08bb7
Kirui Brian <brinkiru@gmail.com>
2024-05-20 10:12:43 +0300

    add a correct 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,8 +2,8 @@
 """
 Flask Web Application Module
 
-This module contains a Flask application instance that listens on all network
-interfaces at port 5000. It defines two routes:
+This module contains a Flask application instance that listens on all
+network interfaces at port 5000. It defines two routes:
 
     1. '/' - returns "Hello HBNB!"
     2. '/hbnb' - returns "HBNB"
@@ -14,25 +14,22 @@ from flask import Flask
 # Initialize the Flask application
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
-    Root route ('/') handler.
-
-    This function is executed when a user accesses the root URL ('/').
-    It returns the string "Hello HBNB!" as a response.
+    Returns the string "Hello HBNB!" when accessing the root '/'
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
-    '/hbnb' route handler.
-
-    This function is executed when a user accesses the URL '/hbnb'.
-    It returns the string "HBNB" as a response.
+    Returns the string "HBNB" when accessing the '/hbnb' path
     """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
