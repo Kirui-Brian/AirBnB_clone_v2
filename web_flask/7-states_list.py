b7380a17d44d0c0a4b76be42f9e14a69191f905b
Kirui Brian <brinkiru@gmail.com>
2024-05-20 13:26:14 +0300

    add correct 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """
 This module contains a Flask instance.
 
-The instance listena on 0.0.0.0, port 5000.
+The instance listens on 0.0.0.0, port 5000.
 """
 
 from flask import Flask, render_template
@@ -11,15 +11,17 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
-    This function returns a HTML page when accessing the '/states_list' path.
-    The page contains a list of all State objects present in DBStorage sorted by name (A->Z).
+    This function returns an HTML page when accessing the '/states_list' path.
+    The page contains a list of all State objects present in DBStorage, sorted by name (A->Z).
     """
     states = storage.all(State)
     sorted_states = sorted(states.values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
+
 
 @app.teardown_appcontext
 def close_session(exception):
@@ -28,5 +30,6 @@ def close_session(exception):
     """
     storage.close()
 
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
