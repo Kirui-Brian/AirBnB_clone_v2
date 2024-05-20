c9f575b8e66598c927e77b1848129298035c6ca2
Kirui Brian <brinkiru@gmail.com>
2024-05-20 10:20:03 +0300

    add a correct 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,7 +9,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
@@ -17,14 +16,12 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     This function returns the string "HBNB" when accessing the '/hbnb' path.
     """
     return "HBNB"
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
@@ -34,7 +31,6 @@ def c_text(text):
     are replaced with a space.
     """
     return "C " + text.replace("_", " ")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
