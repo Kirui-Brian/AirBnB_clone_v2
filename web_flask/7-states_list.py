16e1faed089c27944e66118a8583d183cd3fa732
Kirui Brian <brinkiru@gmail.com>
2024-05-21 11:09:55 +0300

    update 7-states_list.py file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,43 @@
 #!/usr/bin/python3
-"""script to start a flask app on localhost
 """
+Module: flask_app
+
+This module starts a Flask web application on localhost. It uses a storage
+engine to retrieve data and renders templates to display the data.
+
+Routes:
+    /states_list: Displays a list of states using a template.
+"""
+
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def appcontext_teardown(exc=None):
-    """called on teardown of app contexts,
-        for more info on contexts visit
-        -> http://flask.pocoo.org/docs/1.0/appcontext/
+    """
+    Called on teardown of app contexts. For more info on contexts visit:
+    http://flask.pocoo.org/docs/1.0/appcontext/
 
-        Storage.close() closes the sql scoped session or reloads file
-            storage.
+    Storage.close() closes the SQL scoped session or reloads file storage.
     """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def conditional_templating(n=None):
-    """checking input data using templating"""
-    return render_template('7-states_list.html',
-                           states=storage.all("State"))
+    """
+    Displays a list of states using a template.
+
+    Parameters:
+        n: An optional parameter (default is None).
+
+    Returns:
+        A rendered HTML template.
+    """
+    return render_template('7-states_list.html', states=storage.all("State"))
 
 
 if __name__ == '__main__':
