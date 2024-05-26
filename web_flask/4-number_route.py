9dd3a3d0f29e84234f44c46ce7b805363cdcc440
Kirui Brian <brinkiru@gmail.com>
2024-05-26 09:17:09 +0300

    add corrected 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,12 +9,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     This function returns the string "Hello HBNB!" when accessing the root '/'.
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -23,28 +25,34 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """
-    This function returns the string "C " followed by the value of the text variable when accessing the '/c/<text>' path.
+    This function returns the string "C " followed by the value of the
+    text variable when accessing the '/c/<text>' path.
     Underscore symbols in the text variable are replaced with a space.
     """
     return "C " + text.replace("_", " ")
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     """
-    This function returns the string "Python " followed by the value of the text variable when accessing the '/python/<text>' path.
+    This function returns the string "Python " followed by the value of
+    the text variable when accessing the '/python/<text>' path.
     Underscore symbols in the text variable are replaced with a space.
     The default value of the text is "is cool".
     """
     return "Python " + text.replace("_", " ")
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
     """
-    This function returns the string "n is a number" when accessing the '/number/<n>' path.
+    This function returns the string "n is a number"
+    when accessing the '/number/<n>' path.
     This route only works if n is an integer.
     """
     return "{} is a number".format(n)
