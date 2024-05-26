5a2b7f152f45418b5f495db1415c0fb06ba0b61f
Kirui Brian <brinkiru@gmail.com>
2024-05-26 09:14:30 +0300

    add corrected 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,12 +9,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Return the string "Hello HBNB!" when accessing the root '/'.
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -23,21 +25,23 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """
     Return the string "C " followed by the value of the text variable
-    when accessing the '/c/<text>' path. Underscore symbols in the text 
+    when accessing the '/c/<text>' path. Underscore symbols in the text
     variable are replaced with a space.
     """
     return "C " + text.replace("_", " ")
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     """
     Return the string "Python " followed by the value of the text variable
-    when accessing the '/python/<text>' path. Underscore symbols in the text 
+    when accessing the '/python/<text>' path. Underscore symbols in the text
     variable are replaced with a space. The default value of text is "is cool".
     """
     return "Python " + text.replace("_", " ")
